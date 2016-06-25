c81d81c017aa64379f8031ecfcc0fbfdfe147188
mauricio <mromero@mimoni.com>
2016-06-25 03:29:14 -0500

    add

--- api/controllers/PdfgenerateController.js
+++ api/controllers/PdfgenerateController.js
@@ -13,25 +13,39 @@ var request = require('request');
 
 module.exports = {
 	pdf: function (req, res) {
+		var secretToken = 'e18f29e2-6a41-2675-b4cb-4ea553fc8629';
+		var url = 'http://www3.inegi.org.mx//sistemas/api/indicadores/v1//Indicador/6200012020/00000/es/true/json/' + secretToken;
 		var fileName = 'report';
-		var template = '<h1>Reporte {{:number}}</h1>';
-		var data = {
-			number: "1"
-		};
-		jsreport.render({
-			template: {
-				content: template,
-				engine: 'jsrender',
-				recipe: 'phantom-pdf'
-			},
-			data: data
-		}).then(function(resp) {
-    		resp.stream.pipe(res);
-    		resp.stream.pipe(fs.createWriteStream(path.resolve(".")+'/PDF/'+fileName+'.pdf')); /*write pdf file in path*/
-    		res.download(path.resolve(".")+'/PDF/'+filename+'.pdf');						   /*download pdf file*/
-  		}).catch(function(e) {    
-    		res.end(e.message);
-  		});
+		var template = '<meta charset="UTF-8">'+
+		'<style> h1 {font-size: 100%;} </style>'+
+		'<h1>Reporte {{:titleReport}}</h1> '+
+		'<hr>';
+		var nameReport;
+		request(url, function (error, response, body) {
+			var serialData = JSON.parse(body);
+			for (var prop in serialData.Data.Serie[0]) {
+				if (typeof serialData.Data.Serie[0].SourcesPeriod !== 'undefined') {
+					nameReport = serialData.Data.Serie[0].SourcesPeriod;
+				}
+    		}
+			var data = {
+				titleReport: nameReport
+			};
+			jsreport.render({
+				template: {
+					content: template,
+					engine: 'jsrender',
+					recipe: 'phantom-pdf'
+				},
+				data: data
+			}).then(function(resp) {
+	    		resp.stream.pipe(res);
+	    		resp.stream.pipe(fs.createWriteStream(path.resolve(".")+'/PDF/'+fileName+'.pdf')); /*write pdf file in path*/
+	    		//res.download(path.resolve(".")+'/PDF/'+filename+'.pdf');						   /*download pdf file*/
+	  		}).catch(function(e) {    
+	    		res.end(e.message);
+	  		});
+		})
 	},
 };
 
